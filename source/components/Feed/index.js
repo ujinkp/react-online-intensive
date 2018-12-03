135c32f9aacd7566630ac3b38a41bf794045f1b5
Ujin <valikkp@gmail.com>
2018-12-03 22:26:37 +0200

    react-online-lesson-6_1

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,5 +1,6 @@
 // Core
 import React, { Component } from 'react';
+import moment from 'moment';
 
 //Components
 import StatusBar from 'components/StatusBar';
@@ -9,15 +10,34 @@ import Spinner from 'components/Spinner';
 
 //Instruments
 import Styles from './styles.m.css';
+import { getUniqueID } from 'instruments';
 
 
 export default class Feed extends Component {
+    constructor () {
+        super();
+
+        this._createPost = this._createPost.bind(this);
+    }
+    
     state = {
         posts: [
             { id: '123', comment: 'Hi there!', created: 1526825076849 },
             { id: '456', comment: 'Привет', created: 1526825076855 },
         ],
         fetching: false,
+    }
+
+    _createPost (comment) {
+        const post = {
+            id:      getUniqueID(),
+            created: moment.utc(),
+            comment,
+        };
+
+        this.setState(({ posts }) => ({
+            posts: [ post, ...posts ],
+        }));
     }
 
     render() {
@@ -36,7 +56,7 @@ export default class Feed extends Component {
             <section className = { Styles.feed }>
                 <Spinner isSpinning = { fetching } />
                 <StatusBar />
-                <Composer />
+                <Composer _createPost = { this._createPost } />
                 { postsJSX }
             </section>
         );

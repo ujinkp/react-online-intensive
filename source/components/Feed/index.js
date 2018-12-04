5488616e2428aeafc7d867dadd702aa6f008ef89
ujinkp <43612631+ujinkp@users.noreply.github.com>
2018-12-04 15:37:10 +0200

    react-online-lesson-6_2

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -10,7 +10,7 @@ import Spinner from 'components/Spinner';
 
 //Instruments
 import Styles from './styles.m.css';
-import { getUniqueID } from 'instruments';
+import { getUniqueID, delay } from 'instruments';
 
 
 export default class Feed extends Component {
@@ -18,26 +18,78 @@ export default class Feed extends Component {
         super();
 
         this._createPost = this._createPost.bind(this);
+        this._setPostFetchingState = this._setPostFetchingState.bind(this);
+        this._likePost = this._likePost.bind(this);
+
     }
     
     state = {
         posts: [
-            { id: '123', comment: 'Hi there!', created: 1526825076849 },
-            { id: '456', comment: 'Привет', created: 1526825076855 },
+            { 
+                id: '123', 
+                comment: 'Hi there!', 
+                created: 1526825076849, 
+                likes: [] 
+            },
+            { 
+                id: '456', 
+                comment: 'Привет', 
+                created: 1526825076855, 
+                likes: [] },
         ],
         fetching: false,
     }
 
-    _createPost (comment) {
+    _setPostFetchingState (state) {
+        this.setState({
+            fetching: state,
+        });
+    }
+
+    async _createPost (comment) {
+        this._setPostFetchingState(true);
+
         const post = {
             id:      getUniqueID(),
             created: moment.utc(),
             comment,
         };
 
+        await delay(1200);
+
         this.setState(({ posts }) => ({
             posts: [ post, ...posts ],
+            fetching: false,
         }));
+    }
+
+    async _likePost (id) {
+        const { currentUserFirstName, currentUserLastName} = this.props;
+        this._setPostFetchingState(true);
+
+        await delay(1200);
+
+        const newPosts = this.state.posts.map(post => {
+            if (post.id === id) {
+                return {
+                    ...post,
+                    likes: [
+                        {
+                            id:         getUniqueID(),
+                            firstName:  currentUserFirstName,
+                            lastName:   currentUserLastName,
+                        }
+                    ]
+                }
+            }
+
+            return post;
+        });
+
+        this.setState({
+            posts:      newPosts,
+            fetching:   false,
+        })
     }
 
     render() {
@@ -48,6 +100,7 @@ export default class Feed extends Component {
                 <Post
                     key = { post.id }
                     { ...post }
+                    _likePost = { this._likePost }
                 />
             );
         });
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,9 +1,10 @@
 // Core
 import React, { Component } from 'react';
 import moment from 'moment';
-import PropTypes from 'prop-types';
+import { func, string, number, array } from 'prop-types';
 
 //Components
+import Like from 'components/Like';
 import { Consumer } from 'components/HOC/withProfile';
 
 //Instruments
@@ -12,12 +13,15 @@ import Styles from './styles.m.css';
 
 export default class Post extends Component {
     static propTypes = {
-        comment: PropTypes.string.isRequired,
-        created: PropTypes.number.isRequired,
+        _likePost:  func.isRequired,
+        comment:    string.isRequired,
+        created:    number.isRequired,
+        id:         string.isRequired,
+        likes:      array.isRequired,
     }
 
     render() {
-        const { comment, created } = this.props;
+        const { comment, created, _likePost, id, likes } = this.props;
         return (
             <Consumer>
                 {(context) => (
@@ -28,6 +32,12 @@ export default class Post extends Component {
                             {moment.unix(created).format('MMMM D h:mm:ss a')}
                         </time>
                         <p>{comment}</p>
+                        <Like 
+                            _likePost = { _likePost}
+                            id = { id }
+                            likes = { likes }
+                            { ...context }
+                        />
                     </section>
                 )}
             </Consumer>

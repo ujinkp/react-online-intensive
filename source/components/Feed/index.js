b9a10eb884e87278e4cf85636df1b806d748fd94
Ujin <valikkp@gmail.com>
2018-12-11 19:12:22 +0200

    react-online-lesson-10-HW

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -162,6 +162,8 @@ export default class Feed extends Component {
                     classNames = {{
                         enter:       Styles.postInStart,
                         enterActive: Styles.postInEnd,
+                        exit:        Styles.postOutStart,
+                        exitActive:  Styles.postOutEnd,
                     }}
                     key = { post.id }
                     timeout = {{
